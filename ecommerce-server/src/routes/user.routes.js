28f31d930ed166c58be292f6737d590aebbd2134
gabrielNZT <gabrielnunescesarino@gmail.com>
2023-03-11 08:34:27 -0300

    remove user

--- ecommerce-server/src/routes/user.routes.js
+++
@@ -1,6 +0,0 @@
-const router = require('express').Router();
-const userController = require('../controllers/userController');
-
-router.get('/api', userController.index);
-
-module.exports = router;
