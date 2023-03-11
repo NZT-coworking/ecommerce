39b2cc06021347dc13ba44135e3bfef68b851c37
gabrielNZT <gabrielnunescesarino@gmail.com>
2023-03-11 08:34:53 -0300

    product route

--- ecommerce-server/src/routes/product.routes.js
+++ ecommerce-server/src/routes/product.routes.js
@@ -1,8 +1,10 @@
 const router = require("express").Router();
 const upload = require("../config/multer");
-const productController = require("../controllers/productController")
+const productController = require("../controllers/productController");
+const validadeJwtMiddleware = require("../middleware/validadeJwtMiddleware");
 
-router.post('/addProduct', upload.single('file'), productController.addProduct);
+
+router.post('/addProduct', validadeJwtMiddleware, upload.single('file'), productController.addProduct);
 router.get('/getProducts', productController.getProducts);
 
 module.exports = router;
